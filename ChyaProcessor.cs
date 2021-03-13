06c0b91bf32494928315ab037d15ab3867ea801c
Chengkai Yang <yangchengkai@gmail.com>
2021-03-13 17:16:45 +0100

    Add project files.

---
+++ ChyaProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+
+namespace EventHubProcessor
+{
+   public class ChyaProcessor : IEventProcessor
+   {
+      public Task OpenAsync(PartitionContext context)
+      {
+         Console.WriteLine($"SimpleEventProcessor initialized. Partition: '{context.PartitionId}'");
+         return Task.CompletedTask;
+      }
+
+      public Task CloseAsync(PartitionContext context, CloseReason reason)
+      {
+         Console.WriteLine($"Processor Shutting Down. Partition '{context.PartitionId}', Reason: '{reason}'.");
+         return Task.CompletedTask;
+      }
+
+      public Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+      {
+         foreach (var msg in messages)
+         {
+            var data = Encoding.UTF8.GetString(msg.Body.Array, msg.Body.Offset, msg.Body.Count);
+            Console.WriteLine(data + " Partition: " + context.PartitionId);
+         }
+
+         return context.CheckpointAsync();
+      }
+
+      public Task ProcessErrorAsync(PartitionContext context, Exception error)
+      {
+         Console.WriteLine($"Error on Partition: {context.PartitionId}, Error: {error.Message}");
+         return Task.CompletedTask;
+      }
+   }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+
+namespace EventHubProcessor
+{
+   class Program
+   {
+      static async Task Main(string[] args)
+      {
+         var storageConn =
+            "DefaultEndpointsProtocol=https;AccountName=chyastorage;AccountKey=uzMzvbJTKwrvFlwcnRQJrFUal6D4tz2YIgqJvFCDurFJuiGxGr/OHJtPdgbdkXjIa3O8YWKb2yzotRHfJGlQig==;EndpointSuffix=core.windows.net";
+         var blobContainer = "event";
+         var hubConn =
+            "Endpoint=sb://chyaeventhub.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=//JboujWToYmJ2tytgSKApsCgERt2OC4O4/H5uXD7CQ=";
+
+         Console.WriteLine("Event Hub Processor...");
+
+         var processorHost = new EventProcessorHost("chyahub",
+                                                    PartitionReceiver.DefaultConsumerGroupName,
+                                                    hubConn,
+                                                    storageConn,
+                                                    blobContainer
+         );
+
+         await processorHost.RegisterEventProcessorAsync<ChyaProcessor>();
+
+         Console.WriteLine("Receiving. Press ENTER to stop worker.");
+         Console.ReadLine();
+
+         processorHost.UnregisterEventProcessorAsync();
+      }
+   }
+}
